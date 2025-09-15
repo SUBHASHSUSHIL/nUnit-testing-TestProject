7d32d2d14d8e7458648be3b36c945c7db36151bf
SUSHIL THAKUR <mt3028788@gmail.com>
2025-09-15 12:33:14 +0530

    add new class in PrimeUtilityNUnit

---
+++ NUnit-Project/PrimeUtilityNUnit.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_Project
+{
+    [TestFixture]
+    public class PrimeUtilityNUnit
+    {
+
+    }
+}
