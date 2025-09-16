f248c36a1bcec86228c8bf58d13486285697a40a
SUSHIL THAKUR <mt3028788@gmail.com>
2025-09-17 00:06:05 +0530

    employee real world check

---
+++ NUnit-Project/EmployeesTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using nUnit_testing_TestProject.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_Project
+{
+    [TestFixture]
+    public class EmployeesTest
+    {
+        private Employee _employee;
+
+        [SetUp]
+        public void Setup()
+        {
+            _employee = new Employee();
+        }
+
+        [Test]
+        public void AddEmployee()
+        {
+            //Arange
+            _employee = new Employee { Id = 1, Name = "Hello", Email = "hello@gmail.com" };
+
+            //Act
+            _employee.Employees.Add(_employee);
+
+            //Assert
+            Assert.That(_employee.Id, Is.EqualTo(1));
+            Assert.That(_employee.Name, Is.EqualTo("Hello"));
+
+        }
+    }
+}
