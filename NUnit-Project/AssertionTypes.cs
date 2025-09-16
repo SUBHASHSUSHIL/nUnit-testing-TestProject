df75e3d2d03ffc08f5a5d990652726230e95881e
SUSHIL THAKUR <mt3028788@gmail.com>
2025-09-16 23:25:05 +0530

    add in multiple assertion

---
+++ NUnit-Project/AssertionTypes.cs
@@ -0,0 +1,148 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_Project
+{
+    [TestFixture]
+    public class AssertionTypes
+    {
+        private List<string> _fruits;
+        private Dictionary<string, int> _ages;
+        private People _person;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fruits = new List<string> { "Apple", "Banana", "Cherry" };
+            _ages = new Dictionary<string, int>
+            {
+                { "Alice", 30 },
+                { "Bob", 25 },
+                { "Charlie", 35 }
+            };
+            _person = new People
+            {
+                Name = "John Doe",
+                Age = 28,
+                Email = "john@gmail.com"
+            };
+        }
+
+        [Test]
+        public void EqualityAssertion()
+        {
+            string actual = "Hello World";
+            int number = 42;
+
+            Assert.That(actual, Is.EqualTo("Hello World"));
+            Assert.That(number, Is.EqualTo(42));
+            Assert.That(number, Is.Not.EqualTo(100));
+            Assert.That(actual, Is.Not.EqualTo("Goodbye World"));
+            Assert.That(actual, Is.EqualTo("hello world").IgnoreCase);
+            Assert.That(actual.Length, Is.EqualTo(11));
+        }
+
+        [Test]
+        public void NumericAssertions()
+        {
+            double value = 5.5;
+            int number = 7;
+
+            Assert.That(value, Is.GreaterThan(5));
+            Assert.That(value, Is.GreaterThanOrEqualTo(5.5));
+            Assert.That(value, Is.LessThan(6));
+            Assert.That(number, Is.LessThanOrEqualTo(7));
+            Assert.That(value, Is.InRange(5, 9));
+        }
+
+        [Test]
+        public void StringAssertions()
+        {
+            string message = "NUnit is a unit-testing framework for .NET";
+            Assert.That(message, Does.StartWith("NUnit"));
+            Assert.That(message, Does.EndWith(".NET"));
+            Assert.That(message, Does.Contain("unit-testing"));
+            Assert.That(message, Does.Match(@"NUnit.*\.NET"));
+            Assert.That(message, Does.Not.Contain("Java"));
+            Assert.That(message.ToLower(), Is.EqualTo(message.ToLower()).IgnoreCase);
+        }
+
+        [Test]
+        public void CollectionAssertions()
+        {
+            Assert.That(_fruits.Count, Is.EqualTo(3));
+            Assert.That(_fruits, Has.Count.EqualTo(3));
+            Assert.That(_fruits, Does.Contain("Apple"));
+            Assert.That(_fruits, Has.No.Member("Grapes"));
+            Assert.That(_fruits, Is.Ordered);
+            Assert.That(_fruits, Is.Unique);
+        }
+
+        [Test]
+        public void DictionaryAssertions()
+        {
+            Assert.That(_ages, Has.Count.EqualTo(3));
+            Assert.That(_ages, Contains.Key("Alice"));
+            Assert.That(_ages, Does.Not.ContainKey("David"));
+            Assert.That(_ages, Contains.Value(25));
+            Assert.That(_ages, Does.Not.ContainValue(40));
+        }
+
+        [Test]
+        public void TypeAssertions()
+        {
+            object text = "Hello World";
+            List<string> list = new List<string>();
+
+            Assert.That(text, Is.TypeOf<string>());
+            Assert.That(list, Is.InstanceOf<IList>());
+            Assert.That(_person, Is.TypeOf<People>());
+            Assert.That(_person, Is.InstanceOf<People>());
+            Assert.That(_person.Name, Is.InstanceOf<string>());
+            Assert.That(_person.Age, Is.InstanceOf<int>());
+        }
+
+        [Test]
+        public void NullAssertions()
+        {
+            string str = null;
+            object obj = new object();
+            Assert.That(str, Is.Null);
+            Assert.That(obj, Is.Not.Null);
+        }
+
+        [Test]
+        public void BooleanAssertions()
+        {
+            bool condition = true;
+            bool anotherCondition = false;
+            Assert.That(condition, Is.True);
+            Assert.That(anotherCondition, Is.False);
+            Assert.That(5 > 3, Is.True);
+            Assert.That(3 > 5, Is.False);
+        }
+
+        [Test]
+        public void PropertyAssertions()
+        {
+            Assert.That(_person, Has.Property("Name").EqualTo("John Doe"));
+            Assert.That(_person, Has.Property("Age").GreaterThan(20));
+            Assert.That(_person, Has.Property("Email").Contains("@"));
+        }
+
+        [Test]
+        public void ExceptionAssertions()
+        {
+            TestDelegate codeThatThrows = () => throw new InvalidOperationException("Invalid operation occurred");
+            Assert.That(codeThatThrows, Throws.TypeOf<InvalidOperationException>());
+            Assert.That(codeThatThrows, Throws.Exception.Message.EqualTo("Invalid operation occurred"));
+            TestDelegate codeThatDoesNotThrow = () => { var x = 5 + 5; };
+            Assert.That(codeThatDoesNotThrow, Throws.Nothing);
+        }
+    }
+}
--- NUnit-Project/PrimeUtilityNUnit.cs
+++ NUnit-Project/PrimeUtilityNUnit.cs
@@ -4,12 +4,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using nUnit_testing_TestProject.Core;
 
 namespace NUnit_Project
 {
     [TestFixture]
     public class PrimeUtilityNUnit
     {
+        PrimeUtility primeUtility = new PrimeUtility();
 
+        [Test]
+        public void IsPrime_WithPrimeNumber_ReturnsTrue()
+        {
+            var result = primeUtility.IsPrime(5);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsPrime_WithNonPrimeNumber_ReturnsFalse()
+        {
+            var result = primeUtility.IsPrime(4);
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsPrime_WithLessThanTwo_ReturnsFalse()
+        {
+            var result = primeUtility.IsPrime(1);
+            var resultZero = primeUtility.IsPrime(0);
+            Assert.That(result, Is.False);
+            Assert.That(resultZero, Is.False);
+        }
+
+        [Test]
+        public void IsPrime_WithNegativeNumber_ReturnsFalse()
+        {
+            var result = primeUtility.IsPrime(-3);
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        [TestCase(0, false)]
+        [TestCase(1, false)]
+        [TestCase(2, true)]
+        [TestCase(3, true)]
+        [TestCase(4, false)]
+        [TestCase(5, true)]
+        [TestCase(6, false)]
+        [TestCase(7, true)]
+        [TestCase(8, false)]
+        [TestCase(9, false)]
+        [TestCase(10, false)]
+        [TestCase(11, true)]
+        public void IsPrime_ShouldCheckIsPrime(int number, bool expectedResult)
+        {
+            bool result = primeUtility.IsPrime(number);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void ConcatStrings_ShouldConcatenateTwoStrings()
+        {
+            var result = primeUtility.ConcatStrings("Hello", "World");
+            Assert.That(result, Is.EqualTo("Hello World"));
+            Assert.That(result, Does.Contain("hello").IgnoreCase);
+        }
     }
 }
